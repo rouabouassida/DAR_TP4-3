2b1d3b57dd3864403b2c8474467a40d0049c9180
rouabouassida <146998316+rouabouassida@users.noreply.github.com>
2023-11-24 22:30:14 +0100

    Add files via upload

---
+++ DAR_TP4_3/src/client/Client3.java
@@ -0,0 +1,29 @@
+package client;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Client3 {
+    public static void main(String[] args) {
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+            System.out.println("Salut");
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            int serverPort = 1234;
+            @SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+            System.out.println("Name ");
+            String username = scanner.nextLine();
+            while (true) {
+                System.out.println(" message:\t");
+                String message = scanner.nextLine();
+                byte[] sendData = (username + ": " + message).getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+                clientSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ DAR_TP4_3/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ASUS
+ *
+ */
+module DAR_TP4_3 {
+}
